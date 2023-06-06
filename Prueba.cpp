4fca749f873aaa2de52aa6efaf2de6a38f6caea2
Jocelyn Ileana Balderas Sánchez <a01798528@tec.mx>
2023-06-06 09:39:57 -0600

    Creación del main
    Ya funciona iostream y cout. También se puede compilar y correr

---
+++ Prueba.cpp
@@ -0,0 +1,14 @@
+/*
+Autor: Jocelyn Ileana Balderas Sánchez
+Matrícula: A01798528
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hola mundo!" << endl;
+    return 0;
+}
