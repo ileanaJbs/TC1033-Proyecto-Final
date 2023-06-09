de68ad7ce55c594525eb18859427df5bee0df3d2
Jocelyn Ileana Balderas Sánchez <a01798528@tec.mx>
2023-06-09 09:51:14 -0600

    Creación de Autombil.hpp
    Añadí atributos y métodos a la clase Automobil

---
+++ .vscode/Automobil.hpp
@@ -0,0 +1,27 @@
+/*
+Autor: Jocelyn Ileana Balderas Sánchez
+Matrícula: A01798528
+
+Implementación de la clase Automobil
+
+*/
+
+#ifndef AUTOMOBIL_HPP
+#define ATOMOBIL_HPP
+
+class Automobil
+{
+private:
+    double velocidadActual;
+    bool estado;
+
+public:
+    Automobil(); // constructor por default
+    void prender();
+    void apagar();
+    void acelerar();
+    void frenar();
+    void cargarGasolina();
+};
+
+#endif
