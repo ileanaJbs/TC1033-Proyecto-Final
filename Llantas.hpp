2f6862de87d2b453bb4fbedc1bbba8c4e587952d
Jocelyn Ileana Balderas Sánchez <a01798528@tec.mx>
2023-06-09 10:26:22 -0600

    Creación Llantas.hpp
    Añadí la implementación de la clase llantas

---
+++ Llantas.hpp
@@ -0,0 +1,23 @@
+/*
+Autor: Jocelyn Ileana Balderas Sánchez
+Matrícula: A01798528
+
+Implementación de la clase Llantas
+
+*/
+
+#ifndef LLANTAS_HPP
+#define LLANTAS_HPP
+
+class Llantas 
+{
+private:
+double presion;
+
+public:
+Llantas();
+void inflar(int);
+void desinflar();
+};
+
+#endif
