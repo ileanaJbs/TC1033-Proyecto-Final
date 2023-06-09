0080450418c160626609d4e3cfe5ee94c62a5f38
Jocelyn Ileana Balderas Sánchez <a01798528@tec.mx>
2023-06-09 09:56:59 -0600

    Luces.hpp
    Implementación de la clase Luces

--- Luces.hpp
+++ Luces.hpp
@@ -5,3 +5,25 @@ Matrícula: A01798528
 Implementación de la clase Luces
 
 */
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+#ifndef LUCES_HPP
+#define LUCES_HPP
+
+class Luces {
+    private:
+        string lugar;
+        bool estadoLuces;
+
+    public:
+        Luces(); // constructor por default
+        void apagarLuces();
+        void prenderLuces();
+
+};
+
+#endif 
