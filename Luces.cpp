e542fcdfaaa7053c77daf9ec3c21f44cc215f3d8
Jocelyn Ileana Balderas Sánchez <a01798528@tec.mx>
2023-06-15 00:18:06 -0600

    Actualización Luces.cpp
    Terminé la implementación de la clase Luces

--- Luces.cpp
+++ Luces.cpp
@@ -5,3 +5,25 @@ Matrícula: A01798528
 Implementación de la clase Luces
 
 */
+
+#include "Luces.hpp"
+
+Luces::Luces()
+{
+    estadoLuces = false;
+}
+
+void Luces::prenderLuces(bool)
+{
+    estadoLuces = true;
+}
+
+void Luces::apagarLuces(bool)
+{
+    estadoLuces = true;
+}
+
+bool Luces::getEstadoLuces()
+{
+    return estadoLuces;
+}
