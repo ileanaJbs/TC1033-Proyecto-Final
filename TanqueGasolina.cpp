5999d31ec245ee6c751c6e6564ba711af4ea4edf
Jocelyn Ileana Balderas Sánchez <a01798528@tec.mx>
2023-06-15 00:27:45 -0600

    Implementación TanqueGasolina.cpp
    Terminé de implementar la clase TanqueGasolina.cpp

--- TanqueGasolina.cpp
+++ TanqueGasolina.cpp
@@ -6,3 +6,39 @@ Implementación de la clase TanqueGasolina
 
 */
 
+#include "TanqueGasolina.hpp"
+
+TanqueGasolina::TanqueGasolina()
+{
+    nivel = capacidad;
+}
+
+double TanqueGasolina::getNivelGasolina()
+{
+    return nivel;
+}
+
+double TanqueGasolina::getNivelGasolina()
+{
+    return nivel / capacidad;
+}
+
+void TanqueGasolina::cargarTanque(double litros)
+{
+    if (nivel + litros <= capacidad) 
+    {
+        nivel += litros;
+    } else 
+    {
+        nivel = capacidad;
+    }
+}
+
+void TanqueGasolina::gastarGasolina(double litros)
+{
+    if (nivel - litros >= 0) {
+        nivel -= litros;
+    } else {
+        nivel = 0;
+    }
+}
