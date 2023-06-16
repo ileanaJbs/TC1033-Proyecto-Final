e06d55ca7e84c27f1964804e66304f658b9e3b88
Jocelyn Ileana Balderas Sánchez <a01798528@tec.mx>
2023-06-16 00:01:42 -0600

    Actualización Automobil.cpp
    Empecé la implementación de la clase Automobil

--- Automobil.cpp
+++ Automobil.cpp
@@ -5,3 +5,103 @@ Matrícula: A01798528
 Implementación de la clase Automobil
 */
 
+#include "Automobil.hpp"
+
+#include <iostream>
+#include <array>
+
+using namespace std;
+
+Automobil::Automobil()
+{
+    estado = false;
+    velocidadActual = 0;
+}
+
+void Automobil::prenderApagar()
+{
+    estado = !estado;
+    if (estado)
+    {
+        tablero.mostrarMensaje("El automovil se ha encendido");
+    }
+    else
+    {
+        tablero.mostrarMensaje("El automovil se ha apagado");
+    }
+}
+
+void Automobil::acelerar()
+{
+    if (estado)
+    {
+        velocidadActual += 15;
+        tanqueGasolina.gastarGasolina(0.005 * velocidadActual);
+
+        if (velocidadActual > 230)
+        {
+            velocidadActual = 230;
+        }
+
+        if (velocidadActual > 160)
+        {
+            tablero.mostrarMensaje("¡Peligro! Velocidad alta");
+        }
+
+        if (tanqueGasolina.getPorcentajeNivel() < 0.15)
+        {
+            tablero.mostrarMensaje("¡Recordatorio! Llenar el tanque de gasolina");
+        }
+    }
+    else
+    {
+        tablero.mostrarMensaje("El automovil esta apagado. No se puede acelerar");
+    }
+}
+
+void Automobil::frenar()
+{
+    if (estado)
+    {
+        velocidadActual -= 25;
+
+        if (velocidadActual < 0)
+        {
+            velocidadActual = 0;
+        }
+    }
+    else
+    {
+        tablero.mostrarMensaje("El automovil esta apagado. No se puede frenar");
+    }
+}
+
+void Automobil::prenderLuces() 
+{
+
+}
+
+void Automobil::apagarLuces()
+{
+
+}
+
+void Automobil::cargarGasolina(double litros)
+{
+
+}
+
+void Automobil::mostrarTablero()
+{
+
+}
+
+void Automobil::inflarLlanta(int indice, double presion)
+{
+
+}
+
+void Automobil::desinflarLlanta(int indice, double presion)
+{
+    
+}
