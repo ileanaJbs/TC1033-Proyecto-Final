1eebdd124b624f1a3fa979dbcbd46825d90e8ad8
Jocelyn Ileana Balderas Sánchez <a01798528@tec.mx>
2023-06-15 23:39:10 -0600

    Implementación Tablero.cpp
    Actualicé Tablero.cpp

--- Tablero.cpp
+++ Tablero.cpp
@@ -5,3 +5,37 @@ Matrícula: A01798528
 Implementación de la clase Tablero
 */
 
+#include "Llantas.hpp"
+#include "Tablero.hpp"
+#include <iostream>
+
+void Tablero::mostrarEstado(bool encendido, int velocidad, double nivelGasolina, bool lucesEncendidas, array<double, 4> &presionesLlantas)
+{
+    cout << "----- TABLERO -----" << endl;
+    cout << "Estado del automovil: " << (encendido ? "Encendido" : "Apagado") << endl;
+    cout << "Velocidad: " << velocidad << " km/h" << endl;
+    cout << "Nivel de gasolina: " << nivelGasolina << " litros (" << (nivelGasolina / 42.0) * 100.0 << "%)" << endl;
+    cout << "Luces: " << (lucesEncendidas ? "Encendidas" : "Apagadas") << endl;
+
+    cout << "Estado de las llantas: " << endl;
+    for (int i = 0; i < 4; i++)
+    {
+        cout << "Llanta " << (i + 1) << ": " << presionesLlantas[i] << " psi" << endl;
+        /*
+        se realizaron los cálculos a mano para obtener el 20% de 33 (6.6)
+        6.6 + 33 = 39.3 = capacidad máxima permitida
+        33 - 6.6 = 26.4 = capacidad mínima permitida
+        */
+        if (presionesLlantas[i] > 39.6 || presionesLlantas[i] < 26.4)
+        {
+            cout << " (Peligro)" << endl;
+        }
+        cout << "  ";
+    }
+    cout << endl;
+}
+
+void Tablero::mostrarMensaje(const string &mensaje)
+{
+    cout << mensaje << endl;
+}
